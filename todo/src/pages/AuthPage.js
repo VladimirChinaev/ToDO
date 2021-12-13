72f236d0953f32c7938e3576e7d6405a6a4af757
VladimirChinaev <vladimirchinaev21@gmail.com>
2021-12-13 14:14:57 +0300

    add changehandler

--- todo/src/pages/AuthPage.js
+++ todo/src/pages/AuthPage.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export const AuthPage = () => {
+    const [form, setForm] = useState({
+        user: "", email: "", password: ""
+    })
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
     return (
         <div className="login">
             <div className="title">
                 <h1>Welcome!</h1>
             </div>
             <form method="post">
-                <input type="text" name="u" placeholder="Username..." required="required" className="inputAuth" />
-                <input type="text" name="u" placeholder="Email..." required="required" className="inputAuth" />
-                <input type="password" name="p" placeholder="Password..." required="required" className="inputAuth" />
+                <input
+                    type="text"
+                    name="user"
+                    placeholder="Username..."
+                    required="required"
+                    className="inputAuth"
+                    id="user"
+                    onChange={changeHandler} />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email..."
+                    required="required"
+                    className="inputAuth"
+                    id="email"
+                    onChange={changeHandler} />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password..."
+                    required="required"
+                    className="inputAuth"
+                    id="password"
+                    onChange={changeHandler} />
                 <div className="buttonsAuthContainer">
                     <button type="submit" className="buttonsAuth">Sign in</button>
                     <button type="submit" className="buttonsAuth">Sign up</button>
