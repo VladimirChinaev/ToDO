cf791767117df2acc07205fe74d089fa21b5ab99
VladimirChinaev <vladimirchinaev21@gmail.com>
2021-12-13 13:54:31 +0300

    add authpage layuot

--- todo/src/pages/AuthPage.js
+++ todo/src/pages/AuthPage.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+
 export const AuthPage = () => {
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className="login">
+            <div className="title">
+                <h1>Welcome!</h1>
+            </div>
+            <form method="post">
+                <input type="text" name="u" placeholder="Username..." required="required" className="inputAuth" />
+                <input type="text" name="u" placeholder="Email..." required="required" className="inputAuth" />
+                <input type="password" name="p" placeholder="Password..." required="required" className="inputAuth" />
+                <div className="buttonsAuthContainer">
+                    <button type="submit" className="buttonsAuth">Sign in</button>
+                    <button type="submit" className="buttonsAuth">Sign up</button>
+                </div>
+            </form>
         </div>
     )
 }
